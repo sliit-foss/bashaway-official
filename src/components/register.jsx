22077c137e1daf71417071b939bc474d5df506a6
Akalanka Perera <akalankaperera128@gmail.com>
2022-07-13 02:22:29 +0530

    Fix: register button opacity

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -4,7 +4,7 @@ const Register = ({ showDivider = true }) => {
     return (
         <>
             {showDivider && <Divider />}
-            <div className="flex justify-center bg-black/50 backdrop-blur-sm p-4 md:p-8 cursor-pointer hover:bg-[#01050a] transition duration-300">
+            <div className="flex justify-center bg-black/40 backdrop-blur-sm p-4 md:p-7 cursor-pointer hover:bg-[#01050a83] transition duration-300">
                 <span data-heading="Register Now" className="light-sweep font-extrabold font-poppins text-center text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform">
                     Register Now
                 </span>
