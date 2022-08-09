d9720289cf9e30258988ba216d162d678f87ca3c
Supuni Eleesha Randeniya <supuni.randeniya@gmail.com>
2022-08-09 20:45:04 +0530

    Added the countdown

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,17 +1,80 @@
-import { Divider } from "./common"
+import { Divider } from "./common";
+import Countdown from "react-countdown";
 
 const Register = ({ showDivider = true }) => {
+  const Completionist = () => {
     return (
-        <>
-            {showDivider && <Divider />}
-            <div className="flex justify-center bg-gradient-to-r from-transparent via-bg-black/40 to-bg-black/40  backdrop-blur-sm p-4 md:p-7 cursor-pointer hover:bg-[#01050a83] transition duration-300">
-                <span data-heading="Register Now" className="light-sweep font-extrabold font-poppins text-center text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform">
-                    Register Now
-                </span>
-            </div>
-            <Divider />
-        </>
-    )
-}
+      <>
+        {showDivider && <Divider />}
+        <div className="flex justify-center bg-gradient-to-r from-transparent via-bg-black/40 to-bg-black/40  backdrop-blur-sm p-4 md:p-7 cursor-pointer hover:bg-[#01050a83] transition duration-300">
+          <span
+            data-heading="Register Now"
+            className="light-sweep font-extrabold font-poppins text-center text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform"
+          >
+            Register Now
+          </span>
+        </div>
+        <Divider />
+      </>
+    );
+  };
 
-export default Register
+  const renderer = ({ days, hours, minutes, seconds, completed }) => {
+    if (completed) {
+      return <Completionist />;
+    } else {
+      return (
+        <div className="flex justify-center">
+          <div className="flex flex-col p-1  md:p-4 ">
+            <span className="light-sweep font-bold font-poppins text-center text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform">
+              {days.toString().length == 1 ? "0" + days : days}
+            </span>
+            <div>
+              <p className="font-poppins text-white text-sm text-center font-light uppercase">
+                Days
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col p-1  md:p-4">
+            <span className="light-sweep font-bold font-poppins text-center  text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform">
+              {hours.toString().length == 1 ? "0" + hours : hours}
+            </span>
+            <div>
+              <p className="font-poppins text-white text-sm text-center font-light uppercase">
+                Hours
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col p-1  md:p-4">
+            <span className="light-sweep font-bold font-poppins text-center text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform">
+              {minutes.toString().length == 1 ? "0" + minutes : minutes}
+            </span>
+            <div>
+              <p className="font-poppins text-white text-sm text-center font-light uppercase">
+                Minutes
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-col p-1  md:p-4">
+            <span className="light-sweep font-bold font-poppins text-center text-transparent text-5xl md:text-7xl bg-clip-text bg-gradient-to-r from-primary to-secondary p-4 transform">
+              {seconds.toString().length == 1 ? "0" + seconds : seconds}
+            </span>
+            <div>
+              <p className="font-poppins text-white text-sm text-center font-light uppercase">
+                Seconds
+              </p>
+            </div>
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <header>
+      <Countdown date={Date.now() + 3628800000} renderer={renderer} />
+    </header>
+  );
+};
+
+export default Register;
