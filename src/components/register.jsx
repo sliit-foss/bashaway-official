c69a9ff31d8d633e669a432df9fbde942cc1445b
Akalanka Perera <akalankaperera128@gmail.com>
2022-09-13 12:00:32 +0530

    Updated countdown

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -87,7 +87,7 @@ const Register = ({ showDivider = true, comingSoon }) => {
   return (
     <header>
       <Countdown
-        date={(new Date(2022, 8, 14, 7, 30, 0).getTime())}
+        date={(new Date(2022, 8, 15, 19, 0, 0).getTime())}
         renderer={renderer}
         onComplete={() => {
           setRegistration(true);
