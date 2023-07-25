df5a5da3d42781a5fa7335d37aaf68753a12dd73
Akalanka <akalankaperera128@gmail.com>
2023-07-25 22:07:10 +0530

    Fix: deploy installation script

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,6 +1,8 @@
 currentYear=$(date +"%Y")
 
-# copy build files of each year to root
+mkdir -p ./dist
+
+# Copy build files of each year to root
 find "apps" -maxdepth 1 -mindepth 1 -type d | while read -r year; do
     year=$(basename "$year")
     cp ./apps/$year/dist -rf ./dist/$year
@@ -10,7 +12,7 @@ find "apps" -maxdepth 1 -mindepth 1 -type d | while read -r year; do
     fi
 done
 
-# if website for current year doesn't exist in apps, copy build files of previous year to root
+# If website for current year doesn't exist in apps, copy build files of previous year to root
 if [ ! -d "./apps/$currentYear" ]; then
     previousYear=$(($currentYear - 1))
     cp ./dist/$previousYear/** -rf ./dist
