28ae387f846e0038cb387ac202d0b5493e96121b
Akalanka <akalankaperera128@gmail.com>
2023-07-25 22:43:22 +0530

    Debug: added logs to post build script

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -2,9 +2,14 @@ currentYear=$(date +"%Y")
 
 mkdir -p ./dist
 
+cd ./apps/2023
+
+ls -all
+
 # Copy build files of each year to root
 find "apps" -maxdepth 1 -mindepth 1 -type d | while read -r year; do
     year=$(basename "$year")
+    echo $year;
     cp ./apps/$year/dist -rf ./dist/$year
     if [ "$year" -eq "$currentYear" ]; then
         cp ./dist/$year/** -rf ./dist
