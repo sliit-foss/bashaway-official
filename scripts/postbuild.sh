1fdc6bfacafe91959e0b6015b78b127181f4aacc
Akalanka <akalankaperera128@gmail.com>
2023-07-25 22:49:45 +0530

    Debug: removed remote caching

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -2,10 +2,6 @@ currentYear=$(date +"%Y")
 
 mkdir -p ./dist
 
-cd ./apps/2023
-
-ls -all
-
 # Copy build files of each year to root
 find "apps" -maxdepth 1 -mindepth 1 -type d | while read -r year; do
     year=$(basename "$year")
@@ -13,7 +9,7 @@ find "apps" -maxdepth 1 -mindepth 1 -type d | while read -r year; do
     cp ./apps/$year/dist -rf ./dist/$year
     if [ "$year" -eq "$currentYear" ]; then
         cp ./dist/$year/** -rf ./dist
-        #rm ./dist/$year -rf
+        rm ./dist/$year -rf
     fi
 done
 
@@ -21,5 +17,5 @@ done
 if [ ! -d "./apps/$currentYear" ]; then
     previousYear=$(($currentYear - 1))
     cp ./dist/$previousYear/** -rf ./dist
-    #rm ./dist/$previousYear -rf
+    rm ./dist/$previousYear -rf
 fi
