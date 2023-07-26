f80aa30a934a973ba8fa82cbcd5074558675a25e
Akalanka <akalankaperera128@gmail.com>
2023-07-26 20:11:56 +0530

    Chore: synced and fixed history

--- src/components/common/divider.jsx
+++
@@ -1,5 +0,0 @@
-const Divider = ({...props}) => {
-    return <div className="w-full h-[0.75px] bg-nav-links-unselected opacity-20" {...props}/>
-}
-
-export default Divider
--- src/components/common/layout.jsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({ children, title }) => {
-    return (
-        <main className="bg-black font-inter min-h-screen">
-            {children}
-        </main>
-    )
-}
-
-export default Layout
