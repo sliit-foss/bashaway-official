8ae472a581b8ed3168250b57369d2f320012d5e4
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2023-07-29 22:39:30 +0530

    Refactor: override the max width on past event marquee

--- apps/2023/src/components/landing/past-events.jsx
+++ apps/2023/src/components/landing/past-events.jsx
@@ -5,23 +5,25 @@ const PastEvents = () => {
   const handleClickYear = () => window.open(`/2022`, '_blank');
 
   return (
-    <FastMarquee className="bg-black text-xl sm:text-[22px] text-white font-consolas my-6">
-      <div className="flex gap-x-7 items-center mt-3 mb-2 mr-7">
-        {Array(3)
-          .fill(0)
-          .map(() => (
-            <>
-              <div className="flex items-baseline gap-2 cursor-pointer" onClick={handleClickYear}>
-                <span className="text-3xl sm:text-4xl font-light font-cabinet uppercase tracking-tight">
-                  Bashaway 2022
-                </span>
-                <Link className="h-6 sm:h-7" />
-              </div>
-              <Bashaway className="translate-y-0.5 mb-1 sm:mb-2 h-4 sm:h-5" dark />
-            </>
-          ))}
-      </div>
-    </FastMarquee>
+    <div className="pt-6 pb-16">
+      <FastMarquee className="bg-black text-xl sm:text-[22px] text-white font-consolas absolute left-0 right-0">
+        <div className="flex gap-x-7 items-center mt-3 mb-2 mr-7">
+          {Array(10)
+            .fill(0)
+            .map(() => (
+              <>
+                <div className="flex items-baseline gap-2 cursor-pointer" onClick={handleClickYear}>
+                  <span className="text-3xl sm:text-4xl font-light font-cabinet uppercase tracking-tight">
+                    Bashaway 2022
+                  </span>
+                  <Link className="h-6 sm:h-7" />
+                </div>
+                <Bashaway className="translate-y-0.5 mb-1 sm:mb-2 h-4 sm:h-5" dark />
+              </>
+            ))}
+        </div>
+      </FastMarquee>
+    </div>
   );
 };
 
--- apps/2023/src/icons/link.jsx
+++ apps/2023/src/icons/link.jsx
@@ -1,12 +1,6 @@
-export default function Link({ width = 25, height = 28, color = 'white', className }) {
+export default function Link({ color = 'white', className }) {
   return (
-    <svg
-      width={width}
-      height={height}
-      viewBox="0 0 25 28"
-      className={className}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 25 28" className={className} fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M4.25833 27.0093L18.7028 1.99076M18.7028 1.99076L0.944444 6.7491M18.7028 1.99076L23.4611 19.7491"
         stroke={color}
