7e0cd6d6249c5e1496d38575110b503fea8780af
Akalanka <akalankaperera128@gmail.com>
2023-07-29 22:34:48 +0530

    Fix: background render efficiency and tile size

--- apps/2023/src/components/common/background.jsx
+++ apps/2023/src/components/common/background.jsx
@@ -2,11 +2,19 @@ import { useLayoutEffect, useState } from 'react';
 
 const highlightBounds = [0.2, 0.8];
 
+const tileSize = window.innerWidth >= 1280 ? (window.innerWidth >= 1536 ? 60 : 54) : 48;
+
+const maxItemsInRow = Math.floor(window.innerWidth / tileSize);
+
 const Glow = () => {
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
 
   useLayoutEffect(() => {
+    const removeGlow = () => {
+      document.querySelector('.highlighted')?.classList.remove('bg-red-100/80', 'highlighted');
+    };
+
     document.onmousemove = (event) => {
       const e = event;
       setX(e.clientX);
@@ -19,20 +27,21 @@ const Glow = () => {
         (e.clientY > window.innerHeight * highlightBounds[1] && e.clientY < window.innerHeight);
 
       const tiles = document.querySelectorAll('.animated-grid-tile');
-      tiles.forEach((tile) => {
-        if (
-          e.clientX > tile.offsetLeft &&
-          e.clientX < tile.offsetLeft + tile.offsetWidth &&
-          e.clientY > tile.offsetTop &&
-          e.clientY < tile.offsetTop + tile.offsetHeight
-        ) {
-          if (insideHighlightArea) {
-            tile.classList.add('bg-red-100/50');
-          }
-        } else {
-          tile.classList.remove('bg-red-100/50');
-        }
-      });
+      const xIndex = Math.floor(e.clientX / tileSize);
+      const yIndex = Math.floor(e.clientY / tileSize);
+      const tile = tiles[yIndex * maxItemsInRow + xIndex];
+
+      removeGlow();
+
+      if (
+        insideHighlightArea &&
+        e.clientX > tile.offsetLeft &&
+        e.clientX < tile.offsetLeft + tile.offsetWidth &&
+        e.clientY > tile.offsetTop &&
+        e.clientY < tile.offsetTop + tile.offsetHeight
+      ) {
+        tile.classList.add('bg-red-100/80', 'highlighted');
+      }
     };
   });
 
@@ -53,12 +62,16 @@ const Glow = () => {
 const Background = () => {
   return (
     <div className="fixed top-0 left-0 h-screen w-screen flex flex-wrap z-30">
-      {Array(500)
+      {Array(Math.ceil((window.innerHeight * window.innerWidth) / (tileSize * tileSize)))
         .fill(0)
         .map((_, index) => (
           <div
             key={index}
-            className="w-[15vw] h-[15vw] sm:w-[10vw] sm:h-[10vw] md:w-[8vw] md:h-[8vw] xl:w-[5vw] xl:h-[5vw] 2xl:w-[4vw] 2xl:h-[4vw] border border-background opacity-100 blur-[1px] transition-all duration-200 animated-grid-tile"
+            className="border-[0.5px] border-background opacity-100 blur-[1px] transition-all duration-250 animated-grid-tile"
+            style={{
+              height: tileSize,
+              width: tileSize
+            }}
           />
         ))}
       <Glow />
