bf1320ac184db61855d39353fbce84ce80abd8d3
Akalanka <akalankaperera128@gmail.com>
2023-07-29 19:24:40 +0530

    Fix: section badge alignment

--- apps/2023/src/components/common/badges/section.jsx
+++ apps/2023/src/components/common/badges/section.jsx
@@ -5,7 +5,7 @@ const SectionBadge = ({ children, className, ...props }) => {
     <div
       className={twMerge(
         `group flex self-center lg:self-start justify-center items-center rounded-3xl px-5 py-[0.4rem] cursor-default shadow-section-badge
-         bg-white text-black text-[22px] text-center tracking-[3.08px] font-bold uppercase transition-all duration-medium lg:-translate-x-5`,
+        bg-white text-black text-[22px] text-center tracking-[3.08px] font-bold uppercase transition-all duration-medium`,
         className
       )}
       {...props}>
