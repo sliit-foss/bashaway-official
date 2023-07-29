39213022c085c2489562bc2a634bedc9da86cc10
Akalanka <akalankaperera128@gmail.com>
2023-07-29 14:23:18 +0530

    Fix: marquee alignment

--- apps/2023/src/components/landing/marquee.jsx
+++ apps/2023/src/components/landing/marquee.jsx
@@ -1,17 +1,19 @@
+import { Fragment } from 'react';
 import { default as FastMarquee } from 'react-fast-marquee';
+import { twMerge } from 'tailwind-merge';
 import { Bashaway } from '@/icons';
 
 const Marquee = () => {
   return (
     <FastMarquee className="bg-black h-[46px] text-xl sm:text-[22px] text-white font-consolas absolute bottom-0 left-0">
-      <div className="flex gap-x-12 items-center">
+      <div className={twMerge('flex gap-x-12 items-center mr-12')}>
         {Array(3)
           .fill(0)
           .map((_, index) => (
-            <div key={index}>
+            <Fragment key={index}>
               <span>Inter University Scripting and Automation Competition</span>
               <Bashaway className="translate-y-0.5" dark />
-            </div>
+            </Fragment>
           ))}
       </div>
     </FastMarquee>
