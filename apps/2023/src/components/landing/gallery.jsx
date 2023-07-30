829cdf8694b51f5d19f3bed4ae5932d5ccc39bb0
Akalanka <akalankaperera128@gmail.com>
2023-07-30 12:43:59 +0530

    Fix: image scale on animate out

--- apps/2023/src/components/landing/gallery.jsx
+++ apps/2023/src/components/landing/gallery.jsx
@@ -5,7 +5,7 @@ const Gallery = () => {
     <div className="w-full flex flex-col mt-5 py-6 px-10 lg:px-24 gap-y-10">
       <SectionBadge>Gallery</SectionBadge>
       <ComponentStack
-        className="h-[450px]"
+        className="h-[460px]"
         itemStyle={({ index, itemHovered }) =>
           index === 0 && !itemHovered ? 'grayscale-0' : 'grayscale hover:grayscale-0'
         }>
