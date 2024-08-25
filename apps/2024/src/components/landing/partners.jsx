040c02bdd2528722d1df5cbe29f88ada63251060
Thulina Wickramasinghe <thulinawickramasinghe@gmail.com>
2024-08-25 17:12:09 +0530

    Content: add SESC logo

--- apps/2024/src/components/landing/partners.jsx
+++ apps/2024/src/components/landing/partners.jsx
@@ -4,7 +4,8 @@ const Partners = () => {
   return (
     <div className="w-full flex flex-col items-center mt-12 py-6 px-6 lg:px-24 gap-y-8 md:gap-y-14">
       <SectionBadge className="self-center lg:self-center">In partnership with</SectionBadge>
-      <div className="flex justify-center items-center gap-10 md:gap-x-20 transform scale-[0.55] xs:scale-75 md:scale-100">
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-10 md:gap-x-20 transform scale-[0.55] xs:scale-75 md:scale-100">
+        <img src="./assets/images/logos/sesc.png" alt="SESC Logo" className=" h-[56px] translate-y-0.5" />
         <img src="./assets/images/logos/wif.png" alt="WIF Logo" className="w-[144px] h-[60px] translate-y-0.5" />
         <img
           src="./assets/images/logos/mozilla.png"
