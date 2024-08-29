aa8de52dabe4e5e633b4cbc9be15a1b098c94f0c
Thulina Wickramasinghe <thulinawickramasinghe@gmail.com>
2024-08-30 01:13:58 +0530

    Content(sponsors): add knowledge partners

--- apps/2024/src/icons/ghsl.jsx
+++
@@ -1,5 +0,0 @@
-export default function GHSL({ className, ...props }) {
-  return (
-    <img src="./assets/images/logos/ghsl.png" alt="GitHub Community Sri Lanka Logo" className={className} {...props} />
-  );
-}
