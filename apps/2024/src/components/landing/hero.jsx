b751066ba467a9a63992735eb1c18ec423b244b5
Thulina Wickramasinghe <thulinawickramasinghe@gmail.com>
2024-09-05 16:26:24 +0530

    Fix(countdown): adjust spacing and increrase z-index of register btn

--- apps/2024/src/components/landing/hero.jsx
+++ apps/2024/src/components/landing/hero.jsx
@@ -6,7 +6,7 @@ import { CountDown } from '.';
 
 const Hero = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-y-6 lg:gap-y-4 2xl:gap-y-8 min-h-[calc(100lvh-70px)]">
+    <div className="flex flex-col justify-center items-center gap-y-5 lg:gap-y-4 2xl:gap-y-8 min-h-[calc(100lvh-70px)]">
       <Bashaway className="w-[280px] sm:w-[400px] h-[40px] sm:h-[68px] lg:h-[60px] 2xl:h-[70px]" />
       <BodyText className="lg:text-center max-w-5xl px-8">
         A unique competition that keeps the coders around the island on their toes. Welcome to Bashaway {currentYear},
@@ -16,7 +16,7 @@ const Hero = () => {
       <Button
         to={`${registrationLink}`}
         target="_blank"
-        className="mt-1 sm:text-[22px] px-6 py-2 rounded-full tracking-[0.44px]"
+        className="mt-1 sm:text-[22px] px-6 py-2 rounded-full tracking-[0.44px] z-30"
         disabled={!isRegistrationsOpen}>
         {isRegistrationsOpen ? 'Register Now' : 'Registration Closed'}
       </Button>
