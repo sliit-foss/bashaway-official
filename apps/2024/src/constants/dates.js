b7d7e5737b180fb3fe8823fd66e2210ade683dbb
Thulina Wickramasinghe <thulinawickramasinghe@gmail.com>
2024-09-13 09:17:59 +0530

    Feat(countdown): change timer to count till awareness session

--- apps/2024/src/constants/dates.js
+++ apps/2024/src/constants/dates.js
@@ -1,2 +1,3 @@
 export const CURRENT_YEAR = 2024;
 export const TIME_REGISTRATION_CLOSING = 'September 29, 2024 23:59:00';
+export const TIME_AWARENESS_SESSION_START = 'September 13, 2024 19:00:00';
